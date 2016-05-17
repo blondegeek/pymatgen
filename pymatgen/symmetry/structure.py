13e87536593ae95a590a4cf254c998d11b43f14b
blondegeek <blondegeek@gmail.com>
2016-05-17 13:17:24 -0700

    add get distinct bonds first version

--- pymatgen/symmetry/structure.py
+++ pymatgen/symmetry/structure.py
@@ -79,52 +79,89 @@ class SymmetrizedStructure(Structure):
 
         raise ValueError("Site not in structure")
 
-    def get_equiv_by_species(self):
+    def get_equiv_by_specie(self):
         """
         Returns dictionary with { specie : list of equiv positions}
+
+        Note, that this will also work for structures with oxidation states.
+        However, the oxidation state will be part of the index string. Use the
+        remove_oxidation_state method on on the SymmetrizedStructure object
+        and then use this method if you would prefer to not have oxidation
+        states included.
+
+        Returns:
+            { specie : [equiv_index, ...] , ...} 
         """
 
-        equiv_by_species = {}
+        equiv_by_specie = {}
         for i in self.composition.elements:
-            equiv_by_species.update({str(i):[]})
+            equiv_by_specie.update({str(i):[]})
 
         for i in set(self.site_labels):
             site = self[i]
-            equiv_by_species[str(site.specie)].append(i)
+            equiv_by_specie[str(site.specie)].append(i)
 
-        return equiv_by_species
+        return equiv_by_specie
 
-    def get_atom_labels(self):
+    def get_specie_labels(self):
         """
-        Labels site by species and equivalent site number. Useful for generating
+        Labels site by element and equivalent site number. Useful for generating
         traditional crystal structure tables.
 
         The index of the equivalent position for a given element 
-        name gives atom number. For equiv_by_species,
+        name gives atom number. For equiv_by_specie,
               {'Ir':[0,4,6],'O':[8,10]} 
-        get_atom_labels would return atom labels
+        get_specie_labels would return atom labels
               { 0 : 'Ir1', 4 : 'Ir2', 6 : 'Ir3', 8 : 'O1', 10 : 'O2'}
+
+        Note, that this will also work for structures with oxidation states.
+        However, the oxidation state will be part of the string. Use the
+        remove_oxidation_state method on on the SymmetrizedStructure object
+        and then use this method if you would prefer to not have oxidation
+        states included.
+
+        Returns:
+            { equiv_site : label, ...}
         """
 
-        equiv_by_species = self.get_equiv_by_species()
+        equiv_by_specie = self.get_equiv_by_specie()
 
-        atom_labels = {}
+        specie_labels = {}
         for i in set(self.site_labels):
             label = str(self[i].specie)
-            label += str(equiv_by_species[str(self[i].specie)].index(i)+1)
-            atom_labels.update({i : label})
+            label += str(equiv_by_specie[str(self[i].specie)].index(i)+1)
+            specie_labels.update({i : label})
 
-        return atom_labels
+        return specie_labels
 
-#    def get_distinct_bonds(self,r,dr,bonds=None):
-#        """
-#        Returns symmetrically distinct bond lengths up to a certain radius.
-#
-#        """
-#        equiv_by_species = self.get_equiv_by_species
-#        atom_labels = self.get_atom_labels()
-#
-#        if bonds == None:
-#            
-#
+    def get_distinct_bonds(self,r,dr,sigfig=3):
+        """
+        Returns symmetrically distinct bond lengths up to a certain radius.
+
+        Args:
+            r (float): Inner radius of shell.
+            dr (float): Width of shell.
+            sigfig (int):
+                Significant figures to distinguishing between bond lengths.
+        """
+
+        equiv_by_specie = self.get_equiv_by_specie
+        specie_labels = self.get_specie_labels()
+
+        # Get all pairwise species combinations
+        species = map(str,self.composition.elements)
+        #bonds = set(combinations_with_replacement(species,2))
+
+        bonds = set()
+
+        for index in set(self.site_labels):
+            neigh = s.get_neighbors_in_shell(self[index].coords,r,dr,
+                                             include_index=True)
+            for n in neigh:
+                equiv = tuple(sorted([index,self.site_labels[n[2]]]))
+                dist = str.format('{0:.'+str(sigfig)+'f}',round(n[1],sigfig))
+                bonds.update((equiv,dist))
+
+        return bonds
+
 #    def get_distinct_angles(self):
